19d43ce353b7b43a418c9e0ebb2ad29a21f8c205
lyamtorres <lyamtorres@hotmail.com>
2021-02-15 11:49:18 +0100

    Amélioration du code

--- travaux-diriges/td4/3-processus-threads-v1.cpp
+++ travaux-diriges/td4/3-processus-threads-v1.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
-#include <sys/types.h> 
-#include <unistd.h> 
-#include <sys/wait.h> 
-#include <stdlib.h> 
-#include <thread> 
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
 using namespace std;
 
-int i;       
-
-void fils() {
-    i += 10;
-    cout << "Bonjour fils : " << i << endl;
-    i += 20;
-    cout << "Bonjour fils : " << i << endl;
-}
+int i;
 
 int main() {
-    pid_t pid;  
-    i = 0;  
-    thread t(fils);
-    i += 1000;
-    cout << "Bonjour père : " << i << endl;
-    i =+ 2000;
-    cout << "Bonjour père : " << i << endl;
-    t.join();
+    pid_t pid;
+    i = 0;
+    pid = fork();
+    if (pid == -1)
+        perror("Impossible de créer le processus fils");
+    else if (pid == 0) {
+        i += 10;
+        cout << "Bonjour fils : " << i << endl;
+        i += 20;
+        cout << "Bonjour fils : " << i << endl;
+        exit(0);
+    } else {
+        i += 1000;
+        cout << "Bonjour père : " << i << endl;
+        i += 2000;
+        cout << "Bonjour père : " << i << endl;
+        wait(NULL);
+    } 
     return 0;
 }
+
+// Question : Pourquoi le père s'exécute toujours avant le fils ?
