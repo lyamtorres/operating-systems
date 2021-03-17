aeed47485a703b7ffa27b32ceb47b3dad912ce7c
lyamtorres <lyamtorres@hotmail.com>
2021-03-17 14:28:07 +0100

    Changements mineurs

--- travaux-pratiques/tp6/2-fonction-kill.cpp
+++ travaux-pratiques/tp6/2-fonction-kill.cpp
@@ -1,22 +1,37 @@
+// Créer une boucle infinie
+// Écrire bonjour à l'écran
+// Écrire bonsoir à l'écran
+// Une seule réaction pour le signal SIGUSR1
+// Autant de réactions que nécessaire pour le signal SIGUSR2  
+
 #include <iostream>
 #include <unistd.h>
 #include <signal.h>
 
 using namespace std;
 
-void handINT (int signo) { 
-    cout << "reception d'un signal SIGINT\n";
+void handleSigusr1 (int signo) { 
+    cout << "Bonjour" << endl;
+}
+
+void handleSigusr2 (int signo) { 
+    cout << "Bonsoir" << endl;
 }
 
 int main(){ 
-    struct sigaction s;
-    s.sa_handler = handINT; 
-    int retval = sigaction(SIGINT, &s, NULL);
-    if (retval < 0) {
+    struct sigaction s1;
+    struct sigaction s2;
+
+    s1.sa_handler = handleSigusr1;
+    s2.sa_handler = handleSigusr2; 
+    int retval1 = sigaction(SIGUSR1, &s1, NULL);
+    int retval2 = sigaction(SIGUSR2, &s2, NULL);
+
+    if (retval1 < 0 || retval2 < 0) {
         perror("sigaction failed");
         exit(1);
     } 
     cout << "processus : " << getpid() << endl;
-    while(1);
+    while(1); // Boucle infinie
     return 0; 
-}
+} 
