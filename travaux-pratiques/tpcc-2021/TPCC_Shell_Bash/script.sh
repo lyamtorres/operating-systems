a51469dcdacfac81960d68a90a975b6d62f90462
lyamtorres <lyamtorres@hotmail.com>
2021-03-19 10:48:09 +0100

    TPCC terminé

--- travaux-pratiques/tpcc-2021/TPCC_Shell_Bash/script.sh
+++ travaux-pratiques/tpcc-2021/TPCC_Shell_Bash/script.sh
@@ -1,2 +1,16 @@
 #! /usr/bin/bash
 
+if [ $# -eq 1 ]
+then
+    if [ -d $1 ]
+    then
+        VAR=`ls -R  $1`
+        echo $VAR
+    else
+        echo "Ce n'est pas un répertoire" >&2
+        exit 1
+    fi
+else 
+    echo "Vous devez avoir un seul argument" >&2
+    exit 1
+fi
